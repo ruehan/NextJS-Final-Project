30851d72d9ac8f566b60b2d013d96c74163b878b
vendredl <node.js_@kakao.com>
2023-07-17 23:39:24 +0900

    :lipstick: 로그인 여부에 따른 페이지 이동 추가
    로그인 여부에 따른 페이지 이동 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,30 @@
-import React from "react";
+import { useRouter } from "next/router";
+import React, { useEffect } from "react";
+import useSWR from "swr";
 
-export default () => (
-  <div>
-    <h1>Hello</h1>
-  </div>
-);
+const Home = () => {
+  const router = useRouter();
+  const { data: user, error: userError } = useSWR('/api/user');
+
+  useEffect(() => {
+    if (userError) {
+      router.push('/log-in');
+    }
+  }, [userError]);
+
+  if (!user) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user.isLoggedIn) {
+    router.push('/log-in');
+    return <div>Loading...</div>;
+  }
+
+  return <div>Welcome to the home page!</div>; 
+
+};
+
+
+
+export default Home
