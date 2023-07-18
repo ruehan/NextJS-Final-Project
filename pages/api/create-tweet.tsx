ca597d0c90c19b76750c398ae6ff753d31f79eeb
vendredl <node.js_@kakao.com>
2023-07-18 21:43:53 +0900

    :lipstick: 트윗 생성 페이지 및 API 추가
    트윗 생성 페이지 및 API 추가

---
+++ pages/api/create-tweet.tsx
@@ -0,0 +1,46 @@
+// pages/api/tweet.js
+
+import { withIronSession } from 'next-iron-session';
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+const handler = async (req: NextApiRequest, res: NextApiResponse) => {
+  const sessionUser = req.session.get('user');
+
+  console.log(sessionUser)
+
+  if (!sessionUser) {
+    return res.status(401).json({ message: 'Unauthorized' });
+  }
+
+  if (req.method === 'POST') {
+    const { authorId, content } = req.body;
+
+    const tweet = await prisma.tweet.create({
+      data: { 
+        authorId : authorId, 
+        content : content 
+    },
+    });
+
+    // const like = await prisma.like.create({
+    //     data: {
+    //         id: 
+    //     }
+    // })
+
+    res.status(200).json({ message: 'Tweet created', tweet });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+};
+
+export default withIronSession(handler, {
+  password: "apslfjguenmdksjflsnq249567ajsnfgmskd",
+  cookieName: 'my-session-cookie',
+  cookieOptions: {
+    secure: process.env.NODE_ENV === 'production',
+  },
+});
---
+++ pages/create-tweet.tsx
@@ -0,0 +1,66 @@
+
+import { useState } from 'react';
+import { useRouter } from 'next/router';
+import useSWR, { mutate } from 'swr';
+
+const CreateTweetPage = () => {
+  const [content, setContent] = useState('');
+  const router = useRouter();
+  const { data: user } = useSWR('/api/user');
+
+  console.log(user)
+
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+
+    if (!user) {
+      console.error('User not logged in');
+      return;
+    }
+
+    const newTweet = {
+      authorId: user.user.name,
+      content,
+    };
+
+    try {
+      await fetch('/api/create-tweet', {
+        method: 'POST',
+        body: JSON.stringify(newTweet),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+
+      mutate('/api/user'); // Refresh user data
+      setContent(''); // Clear input field
+      router.push('/'); // Redirect to home page
+    } catch (error) {
+      console.error('Error creating tweet:', error);
+    }
+  };
+
+  return (
+    <div className="w-full h-screen flex items-center justify-center ">
+        <div className="max-w-2xl h-5/6 flex-1 border-solid border-2 border-indigo-600 rounded-2xl relative">
+      <h1 className=" text-center text-2xl mt-4">Create Tweet</h1>
+      {user && (
+        <form onSubmit={handleSubmit}>
+            <div>{user.user.name}</div>
+          <div className="flex flex-col w-10/12 h-max justify-center items-center self-center ml-14">
+          <textarea
+            placeholder="What's happening?"
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
+            className="w-full h-96 mt-4 rounded-2xl border-solid border-2 border-black"
+          />
+          <button type="submit">Tweet</button>
+          </div>
+        </form>
+      )}
+    </div>
+    </div>
+  );
+};
+
+export default CreateTweetPage;
