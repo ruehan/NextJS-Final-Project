26791bd8ba1a27a7a19d6a1e421b1f714c69018f
vendredl <node.js_@kakao.com>
2023-07-18 17:37:56 +0900

    :lipstick: 회원가입 임시 페이지 추가
    회원가입 임시 페이지 추가

--- pages/create-account.tsx
+++ pages/create-account.tsx
@@ -27,30 +27,42 @@ const CreateAccountPage = () => {
   };
 
   return (
-    <div>
-      <h1>Create Account</h1>
+    <div className="w-full h-screen flex items-center justify-center ">
+      <div  className="max-w-2xl h-5/6 flex-1 border-solid border-2 border-indigo-600 rounded-2xl bg-black relative">
+      <h1 className="text-white text-center text-2xl mt-4">Create Account</h1>
       <form onSubmit={handleSubmit}>
+        <div className="flex flex-col w-10/12 justify-center items-center self-center ml-14">
         <input
           type="text"
           placeholder="Username"
           value={username}
           onChange={(e) => setUsername(e.target.value)}
+          className="w-3/4 h-12 mt-4 rounded-2xl"
         />
         <input
           type="email"
           placeholder="Email"
           value={email}
           onChange={(e) => setEmail(e.target.value)}
+          className="w-3/4 h-12 mt-4 rounded-2xl"
         />
         <input
           type="password"
           placeholder="Password"
           value={password}
           onChange={(e) => setPassword(e.target.value)}
+          className="w-3/4 h-12 mt-4 rounded-2xl"
         />
         
-        <button type="submit">Create Account</button>
+        <button type="submit" className="bg-white mt-4 rounded-2xl w-2/4 h-12 text-xl">Create Account</button>
+        
+        <div className="text-white flex mt-12">
+                <p>이미 계정이 있으신가요?</p>
+                <a href="/log-in" className="text-blue-500 ml-2">로그인하기</a>
+              </div>
+        </div>
       </form>
+    </div>
     </div>
   );
 };
