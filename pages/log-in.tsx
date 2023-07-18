bdfa51d04cf28ca1c9a2b662a463ba0ee53c7acf
vendredl <node.js_@kakao.com>
2023-07-18 17:37:32 +0900

    :lipstick: 로그인 임시 페이지 추가
    로그인 임시 페이지 추가

--- pages/log-in.tsx
+++ pages/log-in.tsx
@@ -6,10 +6,9 @@ const LogInPage = () => {
   const [password, setPassword] = useState('');
   const router = useRouter();
 
-  const handleSubmit = async (e) => {
+  const handleSubmit = async (e: { preventDefault: () => void; }) => {
     e.preventDefault();
 
-    // Perform login logic
     const response = await fetch('/api/log-in', {
       method: 'POST',
       body: JSON.stringify({ username, password }),
@@ -26,23 +25,37 @@ const LogInPage = () => {
   };
 
   return (
-    <div>
-      <h1>Log In</h1>
-      <form onSubmit={handleSubmit}>
-        <input
-          type="text"
-          placeholder="Username"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button type="submit">Log In</button>
-      </form>
+    <div className="w-full h-screen flex items-center justify-center ">
+        <div className="max-w-2xl h-5/6 flex-1 border-solid border-2 border-indigo-600 rounded-2xl bg-black relative">
+          <h1 className="text-white text-center text-2xl mt-4">Log In</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="flex flex-col w-10/12 justify-center items-center self-center ml-14">
+              <input
+                type="text"
+                placeholder="Username"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+                className="w-3/4 h-12 mt-4 rounded-2xl"
+              />
+              <input
+                type="password"
+                placeholder="Password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-3/4 h-12 mt-4 rounded-2xl"
+              />
+              <button type="submit" className="bg-white mt-4 rounded-2xl w-1/4 h-12 text-xl">로그인</button>
+
+              <div className="text-white flex mt-12">
+                <p>계정이 없으세요?</p>
+                <a href="/create-account" className="text-blue-500 ml-2">회원가입하기</a>
+              </div>
+            </div>
+            
+            
+          </form>
+          
+      </div>
     </div>
   );
 };
