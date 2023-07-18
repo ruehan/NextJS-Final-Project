8cb48379be3212bc30cf28810fc4e760d2d8ba22
vendredl <node.js_@kakao.com>
2023-07-18 21:47:18 +0900

    :lipstick: 트윗 상세보기 페이지 및 API 추가
    트윗 상세보기 페이지 및 API 추가

---
+++ pages/api/tweet/[id].tsx
@@ -0,0 +1,23 @@
+
+import { PrismaClient } from '@prisma/client';
+import { NextApiRequest, NextApiResponse } from "next";
+
+const prisma = new PrismaClient();
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const { id } = req.query;
+
+  if (req.method === 'GET') {
+    const tweet = await prisma.tweet.findUnique({
+      where: { id: Number(id) },
+    });
+
+    if (!tweet) {
+      return res.status(404).json({ message: 'Tweet not found' });
+    }
+
+    res.status(200).json({ tweet });
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
