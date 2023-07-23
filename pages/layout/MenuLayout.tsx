1d104ff9e5a5ccf0c8d195ecd2d1ac2783aeecf6
vendredl <node.js_@kakao.com>
2023-07-23 14:59:46 +0900

    :lipstick: 레이아웃 애니메이션 추가
    레이아웃 애니메이션 추가

--- pages/layout/MenuLayout.tsx
+++ pages/layout/MenuLayout.tsx
@@ -15,19 +15,19 @@ const MenuLayout = () => {
           type="button"
           value="Home"
           onClick={() => router.push('/')}
-          className="w-2/5 h-12 mt-4 rounded-3xl mr-12 font-bold text-2xl"
+          className="w-2/5 h-12 mt-4 rounded-3xl mr-12 font-bold text-2xl hover:bg-gray-200 cursor-pointer duration-300"
         />
         <input
           type="button"
           value="Profile"
           onClick={() => router.push(`/profile/${user.user.name}`)}
-          className="w-2/5 h-12 mt-4 rounded-3xl mr-12 font-bold text-2xl"
+          className="w-2/5 h-12 mt-4 rounded-3xl mr-12 font-bold text-2xl hover:bg-gray-200 cursor-pointer duration-300"
         />
         <input
           type="button"
           value="Tweet"
           onClick={() => router.push('/create-tweet')}
-          className="w-2/5 h-12 mt-4 rounded-3xl border-2 border-gray-300 text-white bg-blue-500 mr-12"
+          className="w-2/5 h-12 mt-4 rounded-3xl border-2 border-gray-300 text-white bg-blue-500 mr-12 cursor-pointer"
         />
         <div className="mr-8 mt-8 absolute bottom-8">
           <div className="font-bold">{user.user.name.toUpperCase()}</div>
