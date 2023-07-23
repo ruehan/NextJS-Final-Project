ae6c1b180ca17dce5122f3d0b4a3e2a0527916c0
vendredl <node.js_@kakao.com>
2023-07-23 16:57:54 +0900

    :zap: .
    .

--- pages/api/tweet/[id].tsx
+++ pages/api/tweet/[id].tsx
@@ -7,17 +7,23 @@ const prisma = new PrismaClient();
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { id } = req.query;
 
-  if (req.method === 'GET') {
-    const tweet = await prisma.tweet.findUnique({
-      where: { id: Number(id) },
-    });
+  console.log(id)
 
-    if (!tweet) {
-      return res.status(404).json({ message: 'Tweet not found' });
-    }
-
-    res.status(200).json({ tweet });
-  } else {
-    res.status(405).json({ message: 'Method Not Allowed' });
+  try {
+    if (req.method === 'GET') {
+        const tweet = await prisma.tweet.findUnique({
+          where: { id: Number(id) },
+        });
+    
+        if (!tweet) {
+          return res.status(404).json({ message: 'Tweet not found' });
+        }
+    
+        res.status(200).json({ tweet });
+      } else {
+        res.status(405).json({ message: 'Method Not Allowed' });
+      }
+  } catch (error) {
+    console.log(error)
   }
 }
